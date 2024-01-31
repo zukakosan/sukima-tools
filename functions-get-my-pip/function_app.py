5c79165e2337df0e23a354f021b44160243c327a
zukako <shizhong.hangxi@gmail.com>
2024-01-31 14:52:14 +0900

    react to fowarded ips

--- functions-get-my-pip/function_app.py
+++ functions-get-my-pip/function_app.py
@@ -9,7 +9,12 @@ def pip(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Request URL: %s', req.url)
     logging.info('Request headers: %s', req.headers)
     logging.info('Request params: %s', req.params)
-    # source_ip = req.headers.get()
-    source_ip = req.headers.get('x-forwarded-for').split(':')[0].split(',')[0]
-    return source_ip
+    # source_ip = req.headers.get('x-forwarded-for')
+    iplist = req.headers.get('x-forwarded-for').split(':')[0].split(',')
 
+    # index -2(not -1) is because Azure API Management IP is added to the end of the iplist
+    source_ip = iplist[len(iplist) - 2]
+
+    # source_ip = req.headers.get('x-forwarded-for').split(':')[0].split(',')[0]
+
+    return source_ip
